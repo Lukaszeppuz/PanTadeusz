6d134c38e391ab1b3c9ee8e608ab5b893a77b8c1
Lukaszeppuz <14602@eppuz.edu.pl>
2024-03-20 11:31:40 +0100

    changed index to php

--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 </head>
 <body>
 
-    
-    
     <header class="container">
         <h1 class="text-center">Pan Tadeusz, czyli ostatni zajazd na Litwie: historia szlachecka z roku 1811 i 1812 we dwunastu księgach wierszem.</h1>
         <h2 class="text-end">Adam Mickiewicz</h2>
@@ -34,6 +32,7 @@
                     <a href="./k11.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i> Księga 11</a>
                     <a href="./k12.html" class="list-group-item list-group-item-action"><i class="bi bi-bookmark-fill"></i> Księga 12</a>
                   </div>
+
             </div>
             <div class="col-9">
                 <img src="./pan-tadeusz.jpg" alt="Pan Tadeusz" class="img-fluid rounded">  
@@ -51,4 +50,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
-
